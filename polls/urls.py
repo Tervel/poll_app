5325887234af51c507435faa5ceb68e6ff04804e
Jordan Anslow <jordananslow@gmail.com>
2015-07-12 21:37:07 +1000

    Refactored to use generic views

--- polls/urls.py
+++ polls/urls.py
@@ -2,14 +2,12 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    # added the word 'specifics'
-    url(r'^specifics/(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
     # eg: /polls/
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.IndexView.as_view(), name='index'),
     # eg: /polls/5/
-    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
     # eg: /polls/5/results/
-    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+    url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
     # eg: /polls/5/vote/
     url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,50 +1,44 @@
-from django.shortcuts import render
-
-# Create your views here.
-from django.http import HttpResponseRedirect, HttpResponse
-from django.core.urlresolvers import reverse
-
-# from django.template import RequestContext, loader 1
-from django.shortcuts import render
-
-# from django.http import Http404 1
 from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.views import generic
 
 from .models import Choice, Question
 
-# def index(request): 1
-#     latest_question_list = Question.objects.order_by('-pub_date')[:5] # Lastest 5 questions
-#     template = loader.get_template('polls/index.html')
-#     context = RequestContext(request, {
-#         'latest_question_list': latest_question_list,
-#         })
-#     return HttpResponse(template.render(context))
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list}
-    return render(request, 'polls/index.html', context)
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
 
-def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+# Not using generic views
+# def index(request):
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     context = {'latest_question_list': latest_question_list}
+#     return render(request, 'polls/index.html', context)
 
-def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+# Not using generic views
+# def detail(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'polls/detail.html', {'question': question})
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+# Not using generic views
+# def results(request, question_id):
+#     response = "You're looking at the results of question %s."
+#     return HttpResponse(response % question_id)
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
-
-# def detail(request, question_id): 1
-#     try:
-#         question = Question.objects.get(pk=question_id)
-#     except Question.DoesNotExist:
-#         raise Http404("Question does not exist")
-#     return render(request, 'polls/detail.html', {'question': question})
-
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
 
 def vote(request, question_id):
     p = get_object_or_404(Question, pk=question_id)
